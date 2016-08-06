2a66a5c68e7d9ce6c2b5233b0fc2727bd89661e3
ingvar73 <ingvar73@gmail.com>
2016-08-06 17:27:04 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$server = 'localhost';
+$user = 'root';
+$password = '';
+$dblink = mysql_connect($server, $user, $password);
+if($dblink)
+echo 'Соединение установлено.';
+else
+die('Ошибка подключения к серверу баз данных.');
+$database = 'howdytest';
+$selected = mysql_select_db($database, $dblink);
+if($selected)
+echo ' Подключение к базе данных прошло успешно.';
+else
+die(' База данных не найдена или отсутствует доступ.');
+?>
